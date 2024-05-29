16bad74359d0c3c4ae832c044df1b4df734c6e4c
Lan Sesughun <sesughun.lan@stu.cu.edu.ng>
2024-05-29 01:53:34 -0700

    Circular progress bar

---
+++ src/Timer.js
@@ -0,0 +1,9 @@
+import { CircularProgressbar, buildStyles } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
+function Timer(){
+    return(
+
+        <div><CircularProgressbar value={60} text={`60%`} /></div>
+    );
+}
+export default Timer;
